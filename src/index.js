fde5924950528230f0916aa36ae80ced91754934
anyhhope <hopeanysimova@yandex.ru>
2023-02-08 20:13:20 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let table = structuredClone(MORSE_TABLE);
+    for(var key in table){
+        let numberKey = "";
+        for(var i = 0; i < key.length; i++){
+            if(key[i] === '.'){
+                numberKey += "10";
+            }
+            else{
+                numberKey += "11";
+            }
+        }
+        table[numberKey] = table[key];
+        delete table[key];
+    }
+    let result = "";
+    while(expr.length != 0){
+        let slice = expr.substr(0, 10);
+        if(slice[0] === "*"){
+            result += " ";
+            expr = expr.slice(10);
+            continue;
+        }
+        while(slice[0] === "0"){
+            slice = slice.slice(1);
+        }
+        for(var key in table){
+            if(slice === key){
+                result += table[key];
+                break;
+            }
+        }
+        expr = expr.slice(10);
+    }
+    return result;
 }
 
 module.exports = {
